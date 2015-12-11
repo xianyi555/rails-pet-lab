8734ef499bc7e25550b232d0c6837a2e04aaf190
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-11 04:23:59 -0800

    add edit/update solution

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -24,4 +24,21 @@ class PetsController < ApplicationController
     @pet = Pet.find_by_id(pet_id)
   end
 
+  def edit
+    pet_id = params[:id]
+    @pet = Pet.find_by_id(pet_id)
+  end
+
+  def update
+    pet_id = params[:id]
+    pet = Pet.find_by_id(pet_id)
+    pet_params = params.require(:pet).permit(:name, :breed)
+    if pet.update_attributes(pet_params)
+      redirect_to pet_path(pet)
+    else
+      flash[:error] = pet.errors.full_messages.join(", ")
+      redirect_to edit_pet_path(pet)
+    end
+  end
+
 end
