8438c2513995168c6c4e36e50a9f853085cf7b12
bgveenstra <b.g.veenstra@gmail.com>
2017-01-24 23:41:46 -0800

    implements pet create without good error handling

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,15 +10,24 @@ class PetsController < ApplicationController
     @pet = Pet.find_by(id: pet_id)
   end
 
-  # TODO: set up *new* method with data for new view
   def new
     owner_id = params[:owner_id]
     @owner = Owner.find_by(id: owner_id)
     @pet = Pet.new
   end
 
-  # TODO: set up *create* method with database interactions for create
-  # TODO: handle save errors and communicate issues to user
+  def create
+    owner_id = params[:owner_id]
+    owner = Owner.find_by(id: owner_id)
+    new_pet = Pet.new(pet_params)
+    if new_pet.save
+      owner.pets << new_pet
+      redirect_to pet_path(new_pet)
+    else
+      # TODO: handle save errors and communicate issues to user
+      render :new
+    end
+  end
 
   private
   def pet_params
