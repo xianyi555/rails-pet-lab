dfb3e465bf71def99af4565d7531cc3c469ed91d
bgveenstra <b.g.veenstra@gmail.com>
2017-01-25 00:09:40 -0800

    add pet appointment routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :owners do
     resources :pets, only: [:index, :new, :create]
   end
-  resources :pets, only: :show
+  resources :pets, only: [:show, :edit, :update, :destroy] do
+    resources :appointments, only: [:new, :create]
+  end
 
 end
