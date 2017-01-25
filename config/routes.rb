443f24dfb2ae0256313cf9274b65a19401486cae
bgveenstra <b.g.veenstra@gmail.com>
2017-01-24 23:21:42 -0800

    add pet routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root "site#index"
 
-  resources :owners
+  resources :owners do
+    resources :pets, only: [:index, :new, :create]
+  end
+  resources :pets, only: :show
 
 end
