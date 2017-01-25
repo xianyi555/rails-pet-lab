88fca27b29c353ef363c6eaa714893c2aaea8bdf
bgveenstra <b.g.veenstra@gmail.com>
2017-01-25 00:54:40 -0800

    undo early change

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ActiveRecord::Base
   # TODO: add association to pets
   has_many :pets
   # TODO: add association to appointments (through pets)
-  has_many :appointments, through: :pets
+
   # TODO: add validations
 
   before_save :normalize_phone_number
