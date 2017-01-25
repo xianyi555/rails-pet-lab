510dd5ea80637615e769248b4f00912ba6801454
Brianna Veenstra <bgveenstra@users.noreply.github.com>
2017-01-25 10:43:11 -0800

    todo

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,8 @@
 class Owner < ActiveRecord::Base
+  # TODO: add association ot pets
+  
+  # TODO: add association to appointments (through pets)
+  
   # TODO: add validations
 
   before_save :normalize_phone_number
