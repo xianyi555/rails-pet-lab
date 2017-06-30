c64d440eae516d224b6312c380f82e61a81a62e8
bgveenstra <b.g.veenstra@gmail.com>
2017-06-30 13:26:17 -0700

    add pet tests

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,16 +4,21 @@ RSpec.describe Pet, type: :model do
   let (:owner) do
     Owner.create({ first_name: "John", last_name: "Doe", email: "john@doe.com", phone: "234-567-8901"})
   end
+
   subject (:pet) do
     age = 37.0
-    Pet.create({name: "Fluffy", breed: "gerbil", date_of_birth: (DateTime.now-age).to_date})
+    Pet.create({name: "Fluffy", breed: "gerbil", date_of_birth: (DateTime.now - age).to_date})
   end
 
   describe Pet do
     # TODO: test that it requires (validates the presence of) name
-    # TODO: that it validates the length of the name is <= 255
+    it { should validate_presence_of(:name) }
 
+    # TODO: that it validates the length of the name is <= 255
+    it { should validate_length_of(:name).is_at_most(255) }
+    
     # TODO: test that it requires breed
+    it { should validate_presence_of(:breed) }
   end
 
 end
